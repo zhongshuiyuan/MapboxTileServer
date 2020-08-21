2515d46e6efdbade1cf78994eda4183987becd63
Philipp Wagner <pwag86@googlemail.com>
2020-08-21 18:52:36 +0200

    Configure Prettier Rules

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
-    <Mapbox />
+    <Mapbox v-bind:style="style" v-bind:center="center" v-bind:zoom="zoom" />
   </div>
 </template>
 
 <script>
-import "mapbox-gl/dist/mapbox-gl.css";
-import Mapbox from "./components/Mapbox.vue";
+import 'mapbox-gl/dist/mapbox-gl.css'
+import Mapbox from './components/Mapbox.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     Mapbox,
   },
   computed: {},
   methods: {
     data: function () {
-      return {};
+      return {
+        map: {
+          style:
+            'http://localhost:9000/static/style/osm_liberty/osm_liberty.json',
+          center: [7.628202, 51.961563],
+          zoom: 14,
+        },
+      }
     },
   },
-};
+}
 </script>
 
 <style>
--- Frontend/src/components/Mapbox.vue
+++ Frontend/src/components/Mapbox.vue
@@ -3,33 +3,47 @@
 </template>
 
 <script>
-import { store, mutations } from "@/globals/store";
-import mapboxgl from "mapbox-gl";
+import { store, mutations } from '@/globals/store'
+import mapboxgl from 'mapbox-gl'
 
 export default {
-  name: "Mapbox",
+  name: 'Mapbox',
+  props: {
+    style: {
+      type: String,
+      required: true,
+    },
+    zoom: {
+      type: Number,
+      default: 14,
+    },
+    center: {
+      type: Object,
+      default: function () {
+        return [7.628202, 51.961563]
+      },
+    },
+  },
   mounted() {
-    // build the style url
-
     // instantiate Mapbox GL
     let m = new mapboxgl.Map({
-      container: "map",
-      style: "http://localhost:9000/static/style/osm_liberty/osm_liberty.json",
-      center: [7.628202, 51.961563],
-      zoom: 14,
-    });
+      container: 'map',
+      style: this.style, //"http://localhost:9000/static/style/osm_liberty/osm_liberty.json",
+      center: this.center, //[7.628202, 51.961563],
+      zoom: this.zoom,
+    })
 
-    m.on("load", function () {
+    m.on('load', function () {
       // TODO
-    });
+    })
 
-    m.on("styledata", function () {
+    m.on('styledata', function () {
       if (!store.mbglIsReady) {
-        mutations.setMbglIsReady(true);
+        mutations.setMbglIsReady(true)
       }
-    });
+    })
   },
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
