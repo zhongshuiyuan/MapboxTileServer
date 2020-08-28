ed434e781f11e38b76728d1833f730d1cd2a1a1a
Philipp Wagner <pwag86@googlemail.com>
2020-08-28 08:38:41 +0200

    Use "line" for Polygon Borders

--- Frontend/src/components/MapboxLayer.vue
+++ Frontend/src/components/MapboxLayer.vue
@@ -17,7 +17,8 @@ export default {
   mounted() {
     this.map.addSource(`geojson_${this.id}`, {
       type: 'geojson',
-      data: this.geojson });
+      data: this.geojson
+    });
 
     this.map.addLayer({
       id: `geojson_polygons_${this.id}`,
@@ -28,21 +29,33 @@ export default {
         'fill-color': ['case', ['has', 'fill'], ['get', 'fill'], '#088'],
         'fill-opacity': ['case', ['has', 'fill-opacity'], ['get', 'fill-opacity'], 0.8]
       },
-      'filter': ['==', '$type', 'Polygon']
+      filter: ['==', '$type', 'Polygon']
     });
-    
+
+    this.map.addLayer({
+      id: `geojson_polygons_lines_${this.id}`,
+      type: 'line',
+      source: `geojson_${this.id}`,
+      paint: {
+        'line-width': 3,
+        // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
+        // to set the line-color to a feature property value.
+        'line-color': ['case', ['has', 'color'], ['get', 'color'], '#088']      },
+      filter: ['==', '$type', 'Polygon']
+    });
+
     this.map.addLayer({
       id: `geojson_points_${this.id}`,
       type: 'circle',
       source: `geojson_${this.id}`,
       paint: {
         'circle-radius': ['case', ['has', 'circle-radius'], ['get', 'circle-radius'], 6],
-        'circle-color': ['case', ['has', 'circle-color'], ['get', 'circle-color'], "#088"]
+        'circle-color': ['case', ['has', 'circle-color'], ['get', 'circle-color'], '#088']
       },
-      'filter': ['==', '$type', 'Point']
+      filter: ['==', '$type', 'Point']
     });
 
-        this.map.addLayer({
+    this.map.addLayer({
       id: `geojson_lines_${this.id}`,
       type: 'line',
       source: `geojson_${this.id}`,
@@ -50,9 +63,9 @@ export default {
         'line-width': 3,
         // Use a get expression (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-get)
         // to set the line-color to a feature property value.
-        'line-color': ['case', ['has', 'color'], ['get', 'color'], "#088"]
+        'line-color': ['case', ['has', 'color'], ['get', 'color'], '#088']
       },
-      'filter': ['==', '$type', 'LineString']
+      filter: ['==', '$type', 'LineString']
     });
   },
   render() {}
